7f57366815355fd4a75ae0ec20cc37ea0ee63fbf
tcely <tcely@users.noreply.github.com>
2026-01-02 16:56:53 -0500

    Rename to `messagesRemaining`

--- src/types.ts
+++ src/types.ts
@@ -37,7 +37,7 @@ export interface ResolveUrlResponse {
 }
 
 export interface WorkerWithLimit extends Worker {
-    messagesLeft: number;
+    messagesRemaining: number;
 }
 
 export interface Task {
